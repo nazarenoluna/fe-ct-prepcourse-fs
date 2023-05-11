3ba520cb54a5d3cc049c0d56471b767d0c6cca1b
nazarenoluna <nazarenogluna12@hotmail.com>
2023-05-11 11:40:15 -0300

    homework 04 ejercicios terminados

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -148,14 +148,14 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
-   if (num1 > num2 && num1 > num3 && Math.sign(num1) === 1 || Math.sign(num1) === 0 ){
+   if ( num1 === 0 && num2 == 0 && num3 === 0){
+      return "Error";
+   }else if (num1 > num2 && num1 > num3 && Math.sign(num1) === 1 || Math.sign(num1) === 0 ){
       return "Numero 1 es mayor y positivo";
    }else if (num1 === Math.sign(-1) || num2 === Math.sign(-1) || num3 === Math.sign(-1)){
       return "Hay negativos"
    }else if (num3 > num1 && num3 > num2){
       return num3 + 1;
-   }else if ( num1 === 0 && num2 == 0 && num3 === 0){
-      return "Error";
    }else {
       return false;
    }
@@ -168,8 +168,16 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
-}
+   if (num < 2 || num === 4){
+      return false;
+   }
+   for (var i=2 ; i < num; i++){
+      if (num % i === 0){
+         return false;
+      }  
+   }return true;
 
+}
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
@@ -177,7 +185,7 @@ function esVerdadero(valor) {
    if (valor === true){
       return "Soy verdadero";
    }else {
-      return false;
+      return "Soy falso";
    }
 }
 
@@ -185,6 +193,11 @@ function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+   if( num.toString().length === 3){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 function doWhile(num) {
@@ -192,6 +205,12 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+   var i=0;
+   do {
+      i = i +1;
+      num+= 5;
+   }while( i < 8);
+   return num;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
